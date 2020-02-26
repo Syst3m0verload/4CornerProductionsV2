d73380bea12d1a33e6fa435f3fe6e8b4eda83578
Zhi En Dylan Quek <s10186935@connect.np.edu.sg>
2020-02-26 12:29:10 +0800

    Add project files.

---
+++ 4CornerProductionsV2/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _4CornerProductionsV2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Route("/portfolio")]
+        public IActionResult Portfolio()
+        {
+            return View();
+        }
+        [Route("/info")]
+        public IActionResult Info()
+        {
+            return View();
+        }
+    }
+}
