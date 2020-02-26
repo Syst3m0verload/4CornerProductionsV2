670f5cf53fd023a74eeee3b47472cce09fb88b31
Zhi En Dylan Quek <s10186935@connect.np.edu.sg>
2020-02-26 16:04:10 +0800

    some stuff

--- 4CornerProductionsV2/Controllers/HomeController.cs
+++ 4CornerProductionsV2/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -10,11 +11,35 @@ namespace _4CornerProductionsV2.Controllers
     {
         public IActionResult Index()
         {
+            List<string> carouselList1 = new List<string>();
+            List<string> carouselList2 = new List<string>();
+            foreach (var files in Directory.GetFiles("wwwroot/media/Home/carousel1"))
+            {
+                FileInfo info = new FileInfo(files);
+                var fileName = Path.GetFileName(info.FullName);
+                carouselList1.Add(fileName);
+            }
+            foreach (var files in Directory.GetFiles("wwwroot/media/Home/carousel2"))
+            {
+                FileInfo info = new FileInfo(files);
+                var fileName = Path.GetFileName(info.FullName);
+                carouselList2.Add(fileName);
+            }
+            ViewBag.carouselList1 = carouselList1;
+            ViewBag.carouselList2 = carouselList2;
             return View();
         }
         [Route("/portfolio")]
         public IActionResult Portfolio()
         {
+            List<string> imageList = new List<string>();
+            foreach (var files in Directory.GetFiles("wwwroot/media/Portfolio/images"))
+            {
+                FileInfo info = new FileInfo(files);
+                var fileName = Path.GetFileName(info.FullName);
+                imageList.Add(fileName);
+            }
+            ViewBag.images = imageList;
             return View();
         }
         [Route("/info")]
