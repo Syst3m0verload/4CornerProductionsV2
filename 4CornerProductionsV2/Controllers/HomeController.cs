e7abd2d0dbf754e2d6230806a93832ea1ae737a7
Zhi En Dylan Quek <s10186935@connect.np.edu.sg>
2020-02-27 23:09:26 +0800

    .

--- 4CornerProductionsV2/Controllers/HomeController.cs
+++ 4CornerProductionsV2/Controllers/HomeController.cs
@@ -4,6 +4,8 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using _4CornerProductionsV2.Models;
+using System.Net.Mail;
 
 namespace _4CornerProductionsV2.Controllers
 {
@@ -46,6 +48,47 @@ namespace _4CornerProductionsV2.Controllers
         public IActionResult Info()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> newClient(Client client)
+        {
+            try
+            {
+                // Email: 4cpnotifications@gmail.com
+                // Password: 4cornerProds
+                SmtpClient smtpClient = new SmtpClient();
+                smtpClient.Credentials = new System.Net.NetworkCredential("4cpnotifications@gmail.com", "4cornerProds");
+                MailMessage message = new MailMessage();
+                MailAddress fromAddress = new MailAddress("4cpnotifications@gmail.com");
+                MailAddress toAddress = new MailAddress("robertbleeme@4cornerproductions.com");
+
+                smtpClient.Host = "smtp.gmail.com";
+                smtpClient.Port = 587;
+                smtpClient.EnableSsl = true;
+                smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+                smtpClient.Timeout = 5000;
+
+                message.From = fromAddress;
+                message.To.Add(toAddress);
+                message.IsBodyHtml = false;
+                message.Subject = "New Client Notification";
+                message.Body = 
+                    "Name: " + client.Name +
+                    "\nEmail: " + client.Email +
+                    "\nPhone: " + client.Phone +
+                    "\nEvent Type: " + client.Event +
+                    "\nComments: " + client.Comment;
+                smtpClient.Send(message);
+                TempData["msg"] = "<script>alert('Success');</script>";
+                return RedirectToAction("Info");
+            }
+            catch
+            {
+                TempData["msg"] = "<script>alert('[Error] Please try again or contact me directly at robertbleeme@4cornerproductions.com');</script>";
+                return RedirectToAction("Info");
+            }
+
         }
     }
 }
---
+++ 4CornerProductionsV2/Models/Client.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _4CornerProductionsV2.Models
+{
+    public class Client
+    {
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Event { get; set; }
+
+        public string Comment { get; set; }
+    }
+}
